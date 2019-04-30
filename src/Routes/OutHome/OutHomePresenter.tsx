f1505add999b70c994a62f608a44253c5b2bdd39
KIMINJOONG <ijboym216@gmail.com>
2019-04-30 21:15:29 +0900

    Login Component and React Helmet

--- src/Routes/OutHome/OutHomePresenter.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { RouteComponentProps } from "react-router";
-
-interface IProps extends RouteComponentProps<any> {}
-
-const OutHomePresenter: React.SFC<IProps> = ({}) => <span>stuff</span>;
-
-export default OutHomePresenter;
