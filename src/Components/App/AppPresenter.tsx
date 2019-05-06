acc77aada53ef63057f6af8aeabd94e930b4ac65
KIMINJOONG <ijboym216@gmail.com>
2019-05-06 16:48:51 +0900

    VerifyPhone Screen

--- src/Components/App/AppPresenter.tsx
+++ src/Components/App/AppPresenter.tsx
@@ -27,7 +27,7 @@ const LoggedOutRoutes: React.SFC = () => (
   <Switch>
     <Route path={"/"} exact={true} component={Login} />
     <Route path={"/phone-login"} component={PhoneLogin} />
-    <Route path={"/verify-phone/:number"} component={VerifyPhone} />
+    <Route path={"/verify-phone"} component={VerifyPhone} />
     <Route path={"/social-login"} component={SocailLogin} />
     <Redirect from={"*"} to={"/"} />
   </Switch>
