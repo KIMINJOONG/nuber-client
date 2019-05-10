a72ff83a1474bcf800a60425ad0b03c0c65dcf88
KIMINJOONG <ijboym216@gmail.com>
2019-05-10 18:34:08 +0900

    SocailLogin with Apollo part One

--- src/Routes/SocailLogin/SocailLoginContainer.tsx
+++ src/Routes/SocailLogin/SocailLoginContainer.tsx
@@ -1,9 +1,31 @@
 import React from "react";
 import SocailLoginPresenter from "./SocailLoginPresenter";
+import { Mutation } from "react-apollo";
+import { facebookConnect, facebookConnectVariables } from "src/types/api";
+import { FACEBOOK_CONNECT } from "./SocailLoginQueries";
+import { RouteComponentProps } from "react-router-dom";
 
-class SocailLoginContainer extends React.Component {
+class LoginMutation extends Mutation<
+  facebookConnect,
+  facebookConnectVariables
+> {}
+
+interface IState {
+  firstName: string;
+  lastName: string;
+  email?: string;
+  fbId: string;
+}
+
+interface IProps extends RouteComponentProps<any> {}
+
+class SocailLoginContainer extends React.Component<IProps, IState> {
   public render() {
-    return <SocailLoginPresenter />;
+    return (
+      <LoginMutation mutation={FACEBOOK_CONNECT}>
+        <SocailLoginPresenter />
+      </LoginMutation>
+    );
   }
 }
 
