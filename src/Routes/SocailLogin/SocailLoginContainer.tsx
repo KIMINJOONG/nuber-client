9d5a9121e1e62fe1933526b0e939391828464b12
KIMINJOONG <ijboym216@gmail.com>
2019-05-10 18:45:32 +0900

    SocailLogin with Apollo part Two

--- src/Routes/SocailLogin/SocailLoginContainer.tsx
+++ src/Routes/SocailLogin/SocailLoginContainer.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import SocailLoginPresenter from "./SocailLoginPresenter";
-import { Mutation } from "react-apollo";
-import { facebookConnect, facebookConnectVariables } from "src/types/api";
-import { FACEBOOK_CONNECT } from "./SocailLoginQueries";
+import { Mutation, MutationFn } from "react-apollo";
 import { RouteComponentProps } from "react-router-dom";
+import { facebookConnect, facebookConnectVariables } from "src/types/api";
+import SocailLoginPresenter from "./SocailLoginPresenter";
+import { FACEBOOK_CONNECT } from "./SocailLoginQueries";
 
 class LoginMutation extends Mutation<
   facebookConnect,
@@ -20,13 +20,32 @@ interface IState {
 interface IProps extends RouteComponentProps<any> {}
 
 class SocailLoginContainer extends React.Component<IProps, IState> {
+  public mutation: MutationFn;
   public render() {
+    const { firstName, lastName, email, fbId } = this.state;
     return (
-      <LoginMutation mutation={FACEBOOK_CONNECT}>
-        <SocailLoginPresenter />
+      <LoginMutation
+        mutation={FACEBOOK_CONNECT}
+        variables={{
+          email,
+          fbId,
+          firstName,
+          lastName
+        }}
+      >
+        {(facebookConnect, { loading }) => {
+          this.mutation = facebookConnect;
+          return <SocailLoginPresenter loginCallback={this.callback} />;
+        }}
       </LoginMutation>
     );
   }
+  public callback = fbData => {
+    this.setState({
+      email: fbData.email
+    });
+    this.mutation();
+  };
 }
 
 export default SocailLoginContainer;
